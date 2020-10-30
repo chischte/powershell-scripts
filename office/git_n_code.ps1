05a71890a98006b2d98e60e3df16309bb02bfe35
michi <chischte@gmx.ch>
2020-10-30 10:17:37 +0100

    Add skript and link to open gitkraken

---
+++ office/git_n_code.ps1
@@ -0,0 +1,11 @@
+
+<#
+cd "C:\Users\michael.wettstein\AppData\Local\gitkraken\"
+start-process "update.exe"
+
+cd "C:\Users\michael.wettstein\AppData\Local\gitkraken\app-7.4.0"
+start-process "gitkraken.exe"
+#>
+
+start-process "C:\Users\michael.wettstein\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Axosoft, LLC\GitKraken.lnk"
+
