86af3e7c97b830b7afb3db4411b31122520a24ee
michi <chischte@gmx.ch>
2020-10-30 17:25:34 +0100

    Improve user information

--- get_cad_number/get_cad_number.ps1
+++ get_cad_number/get_cad_number.ps1
@@ -1,35 +1,25 @@
-﻿# New-Item "C:\Users\$env:UserName\Documents\cad_numbers_$env:UserName.txt"
-
-
+﻿# Define path of "Versuchszeichnungen"
 $GlobalCsvPath = "C:\Users\michael.wettstein\Documents\git\powershell-scripts\get_cad_number\0000-000-XXX_Versuchszeichnungen.csv"
 
 
-# read and show global csv file:
-
+# Read and show global csv file:
 $CsvGlobal = Import-Csv -Path "$GlobalCsvPath" -Delimiter ";"
-
- 
-Write-Output " "
-Write-Output "Meine Zeichnungsnummern:"
 $CsvGlobal | Format-Table -AutoSize
 
 # Get latest project where user worked on:
-
 $CsvGlobalFilterdByUsername = Import-Csv -Path "$GlobalCsvPath" -Delimiter ";" |
 Where-Object ERSTELLER -like $env:UserName |
 Select-Object -Last 1
 $PrevProject = $CsvGlobalFilterdByUsername.PROJEKT
-Write-Output $PrevProject
 
 # Ask user to enter project:
-Write-Output "Bitte Projektname eingeben oder Enter drücken für $PrevProject :"
+Write-Output "Bitte <Projektname> eingeben oder <Enter> drücken für <$PrevProject> :"
 $NewProject = Read-Host 
 if ([string]::IsNullOrEmpty($NewProject)) {
     $NewProject = $PrevProject
 }
-#Ask user to enter Name:
+#Ask user to enter Drawingname:
 $NewDrwName = Read-Host -Prompt 'Bitte Name des Teils oder der Baugruppe eingeben' 
-
 
 # Get latest drawing number
 $CsvGlobal = Import-Csv -Path "$GlobalCsvPath" -Delimiter ";"
@@ -43,34 +33,34 @@ $LatestNumber = $LatestNumber -as [int]
 $NewNumber = $LatestNumber + 1
 $NewNumber = $NewNumber -as [string]
 
-
 # Fill up with trailing zeros
 $ZerosToFillUp = 10 - $NewNumber.Length
 
 for ($i = 0; $i -lt $ZerosToFillUp; $i++) {
     $NewNumber = "0" + $NewNumber
 }
+
 # Add dashes
 $NewNumber = $NewNumber.Insert(7, "-")
 $NewNumber = $NewNumber.Insert(4, "-")
 
-#Create new Entry in Csv
+# Add new number to clipboard
+Set-Clipboard $NewNumber
 
-#$NewDrwDesigner = $env:UserName
+#Create new Entry in Csv
 $NewDrwDate = Get-Date -Format "dddd dd/MM/yyyy HH:mm"
 
 $NewEntryString = "$NewNumber;$NewDrwName;$NewProject;$env:UserName;$NewDrwDate"
 
 $NewEntryString | Add-Content -Path $GlobalCsvPath
 
-# Show Updated (user)-numberlist
+# read and show global csv file:
+$CsvGlobal = Import-Csv -Path "$GlobalCsvPath" -Delimiter ";"
+$CsvGlobal | Format-Table -AutoSize
 
-# write new number to versuchszeichnungen
+Write-Output "Die Zeichnungsnummer für $NewDrwName ist $NewNumber und wurde in die Zwischenablage kopiert."
+Write-Output "---------------------------"
 
-# write new number to cad_numbers_username
+# ...make a function to show number list
 
-# open cad_numbers_username
-
-# add new number to clipboard
-
-# add possibility to get a block of new numbers
+exit
