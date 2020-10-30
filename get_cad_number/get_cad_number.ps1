f98b7047bf93bb73d1da2a2c8f616e240025b615
michi <chischte@gmx.ch>
2020-10-30 14:14:25 +0100

    Create new entry in csv

--- get_cad_number/get_cad_number.ps1
+++ get_cad_number/get_cad_number.ps1
@@ -1,14 +1,32 @@
-﻿#New-Item "C:\Users\$env:UserName\Documents\cad_numbers_$env:UserName.txt"
+﻿# New-Item "C:\Users\$env:UserName\Documents\cad_numbers_$env:UserName.txt"
 
 
+$GlobalCsvPath = "C:\Users\michael.wettstein\Documents\git\powershell-scripts\get_cad_number\0000-000-XXX_Versuchszeichnungen.csv"
 
 
+# PREP read and show global csv file:
 
-#PREP read and show global csv file:
+$P = Import-Csv -Path "$GlobalCsvPath" -Delimiter ";"
 
-$P = Import-Csv -Path "C:\Users\michael.wettstein\Documents\git\powershell-scripts\get_cad_number\0000-000-XXX_Versuchszeichnungen.csv" -Delimiter ";"
-
+ 
+Write-Output " "
+Write-Output "Meine Zeichnungsnummern:"
 $P | Format-Table -AutoSize
+
+
+# Add entry to csv file
+
+$NewDrwNumber=    "0000-000-006"
+$NewDrwName=      "Zeichnung 6"
+$NewDrwProject=   "MHC"
+$NewDrwDesigner=  $env:UserName
+$NewDrwDate=      Get-Date -Format "dddd dd/MM/yyyy HH:mm"
+
+$NewEntryString = "$NewDrwNumber;$NewDrwName;$NewDrwProject;$NewDrwDesigner;$NewDrwDate"
+
+$NewEntryString | Add-Content -Path $GlobalCsvPath
+
+
 
 
 
@@ -17,24 +35,24 @@ $P | Format-Table -AutoSize
 
 
 
-#show local cad numbers
+# show local cad numbers
 
 
-#get default values for username and project
+# get default values for username and project
 
 
-#ask if default are ok
+# ask if default are ok
 
-#enter new part name
+# enter new part name
 
-#get new number
+# get new number
 
-#write new number to versuchszeichnungen
+# write new number to versuchszeichnungen
 
-#write new number to cad_numbers_username
+# write new number to cad_numbers_username
 
-#open cad_numbers_username
+# open cad_numbers_username
 
-#add new number to clipboard
+# add new number to clipboard
 
-#add possibility to get a block of new numbers
+# add possibility to get a block of new numbers
