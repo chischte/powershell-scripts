e3dc6ae777713740a896eb686a3cd2da6cab8128
michi <chischte@gmx.ch>
2020-10-30 17:05:11 +0100

    Basic functionality seems to work

--- get_cad_number/get_cad_number.ps1
+++ get_cad_number/get_cad_number.ps1
@@ -13,19 +13,6 @@ Write-Output " "
 Write-Output "Meine Zeichnungsnummern:"
 $CsvGlobal | Format-Table -AutoSize
 
-
-# Add entry to csv file
-
-$NewDrwNumber = "0000-000-006"
-$NewDrwName = "Zeichnung 6"
-$NewDrwProject = "MHC"
-$NewDrwDesigner = $env:UserName
-$NewDrwDate = Get-Date -Format "dddd dd/MM/yyyy HH:mm"
-
-$NewEntryString = "$NewDrwNumber;$NewDrwName;$NewDrwProject;$NewDrwDesigner;$NewDrwDate"
-
-$NewEntryString | Add-Content -Path $GlobalCsvPath
-
 # Get latest project where user worked on:
 
 $CsvGlobalFilterdByUsername = Import-Csv -Path "$GlobalCsvPath" -Delimiter ";" |
@@ -34,15 +21,14 @@ Select-Object -Last 1
 $PrevProject = $CsvGlobalFilterdByUsername.PROJEKT
 Write-Output $PrevProject
 
-# Ask user to enter project and Name:
+# Ask user to enter project:
 Write-Output "Bitte Projektname eingeben oder Enter drücken für $PrevProject :"
-$CurrentProject = Read-Host 
-if ([string]::IsNullOrEmpty($CurrentProject)) {
-    $CurrentProject = $PrevProject
+$NewProject = Read-Host 
+if ([string]::IsNullOrEmpty($NewProject)) {
+    $NewProject = $PrevProject
 }
-Write-Output $CurrentProject
-
-$CurrentName = Read-Host -Prompt 'Bitte Name des Teils oder der Baugruppe eingeben' 
+#Ask user to enter Name:
+$NewDrwName = Read-Host -Prompt 'Bitte Name des Teils oder der Baugruppe eingeben' 
 
 
 # Get latest drawing number
@@ -60,48 +46,24 @@ $NewNumber = $NewNumber -as [string]
 
 # Fill up with trailing zeros
 $ZerosToFillUp = 10 - $NewNumber.Length
-Write-Output $ZerosToFillUp
 
-for ($i = 0; $i -lt $ZerosToFillUp; $i++)
- {
+for ($i = 0; $i -lt $ZerosToFillUp; $i++) {
     $NewNumber = "0" + $NewNumber
 }
 # Add dashes
-$NewNumber = $NewNumber.Insert(7,"-")
-$NewNumber = $NewNumber.Insert(4,"-")
+$NewNumber = $NewNumber.Insert(7, "-")
+$NewNumber = $NewNumber.Insert(4, "-")
 
-Write-Output $NewNumber
+#Create new Entry in Csv
 
+#$NewDrwDesigner = $env:UserName
+$NewDrwDate = Get-Date -Format "dddd dd/MM/yyyy HH:mm"
 
+$NewEntryString = "$NewNumber;$NewDrwName;$NewProject;$env:UserName;$NewDrwDate"
 
+$NewEntryString | Add-Content -Path $GlobalCsvPath
 
-
-
-
-#$SplitLatestNumberArray = $LatestNumber.Split("-")
-Write-Output $SplitLatestNumberArray[2]
-
-
-
-# Create new entry in global csv
-
-
-
-
-
-
-
-# show local cad numbers
-
-
-# get default values for username and project
-
-
-# ask if default are ok
-
-# enter new part name
-
-# get new number
+# Show Updated (user)-numberlist
 
 # write new number to versuchszeichnungen
 
